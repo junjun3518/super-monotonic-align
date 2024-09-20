cb3b4d27a12ddf81e36d84f362f67fb0ad99fb5e
junhyeok lee <junjun3518@gmail.com>
2024-09-20 15:57:21 -0400

    audio last implementation, test needed

--- super_monotonic_align/__init__.py
+++ super_monotonic_align/__init__.py
@@ -2,11 +2,10 @@ import torch
 from super_monotonic_align.core import maximum_path_triton
 
 @torch.no_grad()
-def maximum_path(value, mask, dtype=torch.float32):
+def maximum_path(value, mask, dtype=torch.float32, max_neg_val=-1e-32, audio_last=True):
     """ Triton optimized version.
     value: [b, t_x, t_y]
     mask: [b, t_x, t_y]
-    skip_mask: [b, t_x]
     """
     # check value is contiguous
     value = value.contiguous()
@@ -15,5 +14,5 @@ def maximum_path(value, mask, dtype=torch.float32):
     path = torch.zeros_like(value, dtype=dtype)
     t_x_max = mask.sum(1)[:, 0].to(torch.int32) 
     t_y_max = mask.sum(2)[:, 0].to(torch.int32)
-    path = maximum_path_triton(path, value, t_x_max, t_y_max)
+    path = maximum_path_triton(path, value, t_x_max, t_y_max, max_neg_val=max_neg_val, audio_last=audio_last)
     return path
--- super_monotonic_align/core.py
+++ super_monotonic_align/core.py
@@ -5,6 +5,69 @@ import triton.language as tl
 
 @triton.jit
 def maximum_path(
+    path, value, text_lengths, audio_lengths,
+    stride_B, stride_T, stride_S,
+    max_neg_val,
+    BLOCK_SIZE_T: tl.constexpr
+    ):
+    batch = tl.program_id(axis=0)
+    path += batch * stride_B
+    value += batch * stride_B
+    text_length = tl.load(text_lengths + batch)
+    audio_length = tl.load(audio_lengths + batch)
+    offs_prev = tl.arange(0, BLOCK_SIZE_T)
+    init = tl.where(offs_prev ==0, tl.load(value), max_neg_val)
+    # for j in range(0,1,1):  # set the first column to max_neg_val without init point
+    tl.store(value + offs_prev * stride_T, init, mask=offs_prev < text_length)
+    for j in range(1, audio_length, 1):
+        v_cur= tl.load(value + (offs_prev) * stride_T + (j-1) * stride_S, 
+                       mask=(offs_prev < text_length), other=max_neg_val)
+        v_prev =tl.load(value + (offs_prev-1) * stride_T + (j-1) * stride_S,
+                        mask=(0 < offs_prev) & (offs_prev < text_length), other=max_neg_val)
+        # compare v_cur and v_prev, and update v with larger value
+        v = (tl.maximum(v_cur, v_prev) + tl.load(value + (offs_prev) * stride_T + j * stride_S,
+                                                 mask=(offs_prev < text_length)))
+        tl.store(value + (offs_prev) * stride_T + j * stride_S, v,
+                 mask=(offs_prev < text_length))
+
+    index = text_length-1
+    for j in range(audio_length-1,-1,-1):
+        tl.store(path + (index) * stride_T + j * stride_S, 1)
+        if (index > 0): # (index == j) is not checked due to max_neg_val init
+            v_left = tl.load(value+ (index) * stride_T + (j-1) * stride_S)#.to(tl.float32)
+            v_leftdown =  tl.load(value+(index-1) * stride_T + (j-1) * stride_S)#.to(tl.float32)
+            if (v_left < v_leftdown):
+                index += - 1
+            
+                        
+@torch.no_grad()
+def maximum_path_triton(path, value, x_lengths, y_lengths, max_neg_val=-1e32, audio_last=True):
+    if audio_last:
+        B,T,S = path.shape
+        stride_B = T*S
+        stride_T = S
+        stride_S = 1
+        text_lengths, audio_lengths = x_lengths, y_lengths
+    else:
+        B,S,T = path.shape
+        stride_B = T*S
+        stride_S = T
+        stride_T = 1
+        audio_lengths, text_lengths = x_lengths, y_lengths
+
+    BLOCK_SIZE_T = max(triton.next_power_of_2(T), 16)
+    num_warps = 1 # Need to be 1 to prevent wrong output by slicing the operation
+    with torch.cuda.device(value.device.index):
+        maximum_path[(B, )](
+            path, value, text_lengths, audio_lengths, 
+            stride_B, stride_T, stride_S,
+            max_neg_val = max_neg_val,
+            num_warps = num_warps,
+            BLOCK_SIZE_T = BLOCK_SIZE_T)
+    return path
+
+@triton.jit
+def maximum_path_old(
     path, value, t_x, t_y,
     B, T, S,
     max_neg_val,
@@ -37,16 +100,15 @@ def maximum_path(
             
                         
 @torch.no_grad()
-def maximum_path_triton(path, value, t_x, t_y, max_neg_val=-1e32):
+def maximum_path_triton_old(path, value, t_x, t_y, max_neg_val=-1e32):
     B,T,S = path.shape
     BLOCK_SIZE_X = max(triton.next_power_of_2(T), 16)
     num_warps = 1 # Need to be 1 to prevent wrong output by slicing the operation
     with torch.cuda.device(value.device.index):
-        maximum_path[(B, )](
+        maximum_path_old[(B, )](
             path, value, t_x, t_y, 
             B, T, S,
             max_neg_val = max_neg_val,
             num_warps = num_warps,
             BLOCK_SIZE_X = BLOCK_SIZE_X)
     return path
-
